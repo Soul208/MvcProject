da5d9e986edbdd8c1320a9b879b22a16f447152f
Soul70 <99690641+Soul70@users.noreply.github.com>
2025-04-20 20:40:44 +0300

    update_last2

--- Demo.DataAccess/Data/Migrations/20250415180612_test.Designer.cs
+++ Demo.DataAccess/Data/Migrations/20250415180612_test.Designer.cs
@@ -4,6 +4,7 @@ using Demo.DataAccess.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Demo.DataAccess.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250415180612_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -52,7 +55,7 @@ namespace Demo.DataAccess.Data.Migrations
                     b.Property<int>("LastModifiedBy")
                         .HasColumnType("int");
 
-                    b.Property<DateTime?>("LastModifiedOn")
+                    b.Property<DateTime>("LastModifiedOn")
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Name")
@@ -111,11 +114,7 @@ namespace Demo.DataAccess.Data.Migrations
                     b.Property<int>("LastModifiedBy")
                         .HasColumnType("int");
 
-<<<<<<< HEAD
-                    b.Property<DateTime?>("LastModifiedOn")
-=======
                     b.Property<DateTime>("LastModifiedOn")
->>>>>>> 60bbecd37a2852f45482ab8198784b40aac60716
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Name")
